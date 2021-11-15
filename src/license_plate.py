b12a68de60181647be04e347b90a5990ebb04599
rachel653 <r.webb@alumni.ubc.ca>
2021-11-15 12:56:26 -0800

    detection

---
+++ src/license_plate.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+# import imutils
+
+class License_Plate():
+
+	def __init__(self):
+		self.cardetected = False
+		self.license_in_frame = False
+		self.ymin = 0
+		self.ymax = 100
+		self.xmin = 0
+		self.xmax = 100
+
+	def read_number(self,image):
+		hgt, wdt,_ = image.shape
+		canny = cv2.Canny(image, 50, 120)
+		rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		# Grayscale
+		gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+		# Canny edges
+		edged = cv2.Canny(gray, 30, 200)
+		# Finding Contours
+		contour, hier = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+		# All contours
+		cv2.drawContours(rgb, contours, -1, (0,255,0), 3)
+	
+		return rgb
+
+	def find_Car(self,image):
+		self.cardetected = False
+		image = image[350:710,0:1270]
+		canny = cv2.Canny(image, 50, 120)
+		hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+		gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+		# blue values for cars below
+		# mask = cv2.inRange(hsv, np.array([120,40,2]) , np.array([126,255,255]))
+		mask = cv2.inRange(hsv, np.array([0,0,98]) , np.array([0,0,220]))
+		im,contours,hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+		contours = sorted(contours, key=cv2.contourArea, reverse=True)
+		if contours:
+			self.cardetected = True
+			(x_min, y_min, box_width, box_height) = cv2.boundingRect(contours[0])
+			cv2.rectangle(image, (x_min - 15, y_min -15), (x_min + box_width + 15, y_min + box_height + 50),(0,255,0), 4)
+			self.ymin = y_min-15
+			self.ymax = y_min+box_height + 50
+			self.xmin = x_min -15
+			self.xmax = x_min + box_width + 15
+			return (image,self.cardetected)
+		return (image, self.cardetected)
+
+	def get_license_region(self,image):
+		self.license_in_frame = False
+		if (self.cardetected == True):
+			imagecopy = image[self.ymin:self.ymax,self.xmin:self.xmax]
+			hsv = cv2.cvtColor(imagecopy,cv2.COLOR_BGR2HSV)
+			if hsv is None:
+				return (image,self.license_in_frame)
+			
+			# mask = cv2.inRange(hsv, np.array([20,20,70], dtype = "uint8") , np.array([25,25,255],dtype = "uint8"))
+			# bitwise = cv2.bitwise_and(imagecopy,imagecopy,mask=mask)
+			# ratio_white = cv2.countNonZero(mask)/(imagecopy.size)
+			# percentage_white = (ratio_white * 100)
+			# if (percentage_white>40):
+			# 	self.license_in_frame = True
+			return (imagecopy, self.license_in_frame)
+		return (image,self.license_in_frame)
+
+	
+
+
+
+
+
+
+
+
+		
+
--- src/robot_controller.py
+++ src/robot_controller.py
@@ -13,11 +13,14 @@ from geometry_msgs.msg import Twist
 
 from matplotlib import pyplot as plt
 import numpy as np
+from std_msgs.msg import Time
+from rosgraph_msgs.msg import Clock
 
 #Our class imports
 from lanes_class import Lane_Detection
 from lane_controller import Lane_Controller
 from driver import Driver
+from license_plate import License_Plate
 
 class controller():
 
@@ -25,17 +28,25 @@ class controller():
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
         self.vel_pub = rospy.Publisher("/R1/cmd_vel/", Twist, queue_size=1)
+        self.license = rospy.Publisher("/license_plate",String,queue_size=10)
+        time = rospy.get_time()
         rate = rospy.Rate(2)
         self.twist = Twist()
         self.Lane_Detection = Lane_Detection()
         self.Driver = Driver()
+        self.License_Plate = License_Plate()
+        self.timer_starter = False
 
     def callback(self,data):
+        if rospy.get_time() == 60:
+            self.license.publish(str('idk,idk,0,AB65'))
+        if self.timer_starter == False:
+            self.license.publish(str('idk,idk,-1,9927'))
+            self.timer_starter = True
         try:
-              cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
-              print(e)
-
+            print(e)
         combo_image, hl, left_lane_bottom, right_lane_bottom = self.Lane_Detection.process_image(cv_image)
         forward_velocity, angular_velocity = self.Driver.controller(left_lane_bottom, right_lane_bottom, 'L')
 
@@ -51,13 +62,25 @@ class controller():
             fontScale,
             fontColor,
             lineType)
+        gray = cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
+        edged = cv2.Canny(gray, 30, 200)
+        ret,thresh = cv2.threshold(gray,127,255,0)
+
+        image2,car_found = self.License_Plate.find_Car(cv_image)
+        if(car_found == True):
+            image3,license_found = self.License_Plate.get_license_region(image2)
+            cv2.imshow("cropped",image3)
+            cv2.waitKey(3)
+
 
         self.twist.linear.x = forward_velocity
         self.twist.angular.z = angular_velocity
-        self.vel_pub.publish(self.twist)
+        # self.vel_pub.publish(self.twist)
 
         cv2.imshow("Image window", combo_image)
-        # cv2.imshow("Canny", self.Lane_Detection.canny(cv_image))
+        cv2.imshow("contours",edged)
+        cv2.imshow("contours2",image2)
+        
         cv2.waitKey(3)
 
 
