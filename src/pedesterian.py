066c0670185ecf3fe35f0e8a347f658bf856ccc7
Jack Patchell <jackpatchell222@gmail.com>
2021-11-28 11:08:08 -0800

    a

---
+++ src/pedesterian.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+class Ped_Detection:
+	def __init__(self):
+		pass
+
+	def hsv_filter(self, image):
+		hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+		mask = cv2.inRange(hsv, np.array([91,54,49]) , np.array([113,138,110]))
+		return mask
+
+	def get_pedesterian_location(self, mask):
+		im, contours, hier = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+		contours = sorted(contours, key=cv2.contourArea, reverse=True)
+		print(contours)
+		color_version = cv2.merge((mask, mask, mask))
+		(x_min, y_min, width, height) = cv2.boundingRect(contours[0])
+		cv2.circle(color_version, (x_min, y_min), 25, (0, 255, 0), -1)
+		cv2.imshow("mask", color_version)
+
+		return (x_min, y_min)
+
+	def drive_or_not(self, image):
+		(x, y) = self.get_pedesterian_location(image)
+		if x > 500 and x < 850:
+			return True
+		else:
+			return False
